6ccbbb896ae325e4de4cf6a8f01d0bb3ae820e71
LGZ <liguangzheng998@hotmail.com>
2023-12-17 14:18:20 -0500

    work on mosh tutorial

--- mosh-exercise/index.js
+++ mosh-exercise/index.js
@@ -6,3 +6,18 @@ console.log(myname);
 const interest_rate = 0.05;
 // interest_rate = 1; // error
 console.log(interest_rate);
+// object
+let person = {
+    name: "Mosh",
+    age: 30
+}
+// array
+let arr = [1, 2, "three"]
+console.log(arr)
+
+// function
+function square(num) {
+    let ans = num * num
+    console.log(ans)
+    return ans
+}

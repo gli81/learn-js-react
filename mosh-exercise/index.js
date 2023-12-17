c0964e5aed100dca6dc95c673b2e2d85bd0dda17
LGZ <liguangzheng998@hotmail.com>
2023-12-17 13:58:01 -0500

    work on mosh tutorial

--- mosh-exercise/index.js
+++ mosh-exercise/index.js
@@ -1,2 +1,8 @@
 console.log("Hello World!")
 console.log("hihihi")
+// let, not var
+let myname = "uuu";
+console.log(myname);
+const interest_rate = 0.05;
+// interest_rate = 1; // error
+console.log(interest_rate);
